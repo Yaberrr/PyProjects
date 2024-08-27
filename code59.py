605fcd4e77696d52a15e1148d597f8495b211d9e
tangyabo <13316022418@163.com>
2024-08-27 18:15:43 +0800

    perf:code59

--- code59.py
+++ code59.py
@@ -1,43 +1,31 @@
 class Solution:
     def generateMatrix(self, n: int) -> list[list[int]]:
-        #横轴及纵轴最大最小值
-        arr:list = [0,0,n,n]
-        #起始位置
-        point:list = [0,0]
         result:list = [[0 for _ in range(n)] for _ in range(n)]
-        m:int = 0
-        while m < n:
-            for i in range(4):
-                isX:bool = i%2 == 0
-                begin:int = point[0] if isX else point[1]
-                end:int = (arr[i]+2)%4
-                const:int = (arr[i]+1)%4
-                step:int = 1
-                if i >= 2:
-                    step = -1
-                    temp = begin
-                    begin = end 
-                    end = temp
-                for j in range(begin,end,step):
-                    m+=1
-                    if isX:
-                        result[const][j] = m
-                        point[0] = end
-                    else:
-                        result[j][const] = m
-                        point[1] = end         
-                arr[i] += step
-                
-        return result
+        right,bottom,left,top = n,n,0,0
+        x,y = 0,0
+        value = 0
+        while value <= n:
+            for x in range(x, right+1):
+                result[y][x] = value
+                value += 1
+            top+=1
             
-                
+            for y in range(y, bottom+1):
+                result[y][x] = value 
+                value+=1
+            right-=1
             
+            for x in range(x,left-1,-1):
+                result[y][x] = value 
+                value+=1
+            bottom-=1
+            
+            for y in range(y,top-1,-1):
+                result[y][x] = value 
+                value+=1 
+            left+=1                    
                 
-                            
-                
-                
-                    
+        return result  
                     
                 
-            
             
